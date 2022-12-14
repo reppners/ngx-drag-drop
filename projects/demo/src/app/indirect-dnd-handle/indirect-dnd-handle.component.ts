8a2294c10bf867f7b5676439fdf8813bec5a20e1
Christof Fritz <christof.fritz@mankido.de>
2022-12-14 22:42:30 +0100

    Style fixes to make it look nice again after flex-layout has been yanked out and angular material updated

--- projects/demo/src/app/indirect-dnd-handle/indirect-dnd-handle.component.ts
+++ projects/demo/src/app/indirect-dnd-handle/indirect-dnd-handle.component.ts
@@ -1,9 +1,12 @@
-import { Component } from "@angular/core";
+import {Component, HostBinding} from "@angular/core";
 
 @Component( {
   selector: "dnd-indirect-handle",
   templateUrl: "./indirect-dnd-handle.component.html",
-  styleUrls: [ "./indirect-dnd-handle.component.scss" ]
+  styleUrls: [ "./indirect-dnd-handle.component.scss" ],
 } )
 export class IndirectDndHandleComponent {
+  @HostBinding('class.drag-handle') get dragHandle() {
+    return true;
+  }
 }
